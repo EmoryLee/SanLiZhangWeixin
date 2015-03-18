75d010a51db39d29ff8ab37db5a7b41e1a979b45
EmoryLee <690379823@qq.com>
2015-03-18 10:27:41 +0800

    update weixin.js

--- cloud/weixin.js
+++ cloud/weixin.js
@@ -47,7 +47,9 @@ var queryContact = function(msg, cb) {
 	query.equalTo("CName", cName);
 	query.first().then(
 		function(obj) {
-			var cont = "不知道你啥意思~~";
+			var arr = ['不知道你啥意思~~', '你想干嘛~~', '是不是很無聊~~', '想請我吃飯？抱歉，沒時間~~'];
+			arr.sort(function(){return Math.random()-0.5;});
+			var cont = arr.slice(0,1);
 			if (obj) cont = cName + '的手機號是: ' + obj.get("MobiPhone") + '';
 			var result = {
 				xml: {
